ed8635ced8634dece1c4069b0b753d45d83736a5
OrangeCrush <friederichsm@uwplatt.edu>
2013-01-17 23:53:15 -0600

    precompile styles.css

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,6 @@ module TeamRocket
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'     
     config.assets.initialize_on_precompile = false #heroku
+    config.assets.precompile += %w( styles.css  )
   end
 end
