7c0c794d03c5f86366a825392530a778a4efebef
OrangeCrush <friederichsm@uwplatt.edu>
2013-01-17 23:35:28 -0600

    heroku

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,6 @@ module TeamRocket
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'     
-    #config.assets.initialize_on_precompile = false #heroku
+    config.assets.initialize_on_precompile = false #heroku
   end
 end
